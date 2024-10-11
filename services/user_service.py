e5c0e9890b78bebd6ac73ee1f357bafd3c9fdd2a
Brenner Fagundes <brenner.fagundes@sanar.com>
2024-10-11 10:36:05 -0300

    lint: removendo arquivos desnecessarios

--- services/user_service.py
+++
@@ -1,8 +0,0 @@
-from repositories.user_repository import UserRepository
-
-class UserService:
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
-
-    def get_all_users(self):
-        return self.user_repository.get_all_users()
